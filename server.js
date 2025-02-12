4e60a039e095575c3bb2f2f0ac433cefc84587ac
Arash <arashsaleh798@gmail.com>
2025-02-12 15:43:26 -0800

    exercise 2 complete

--- server.js
+++ server.js
@@ -58,4 +58,14 @@ app.get('/', (req, res) => {
   });
 });
 
+app.get('/menu', (req, res)=> {
+    res.render('menu.ejs', {
+        menu: RESTAURANT.menu,
+
+    });
+    
+
+});
+
+
 app.listen(3000);
